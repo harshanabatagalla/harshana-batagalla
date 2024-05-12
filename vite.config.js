28a73dab770293381040ba8b7d2b9168f61ab35c
Harshana Batagalla <harshanabatagalla@gmail.com>
2024-05-12 23:37:08 +0530

    build: Update build configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [
     react()
   ],
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // Set the output directory
+    emptyOutDir: true, // Clear the output directory before building
+    assetsDir: '.', // Serve assets from the root directory
+  },
 })
