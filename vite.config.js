2212dd59bf97772fe85359402d1f85e2de21d553
Harshana Batagalla <harshanabatagalla@gmail.com>
2024-05-12 23:45:17 +0530

    chore: Remove unused GitHub Pages deployment workflow and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,9 +6,4 @@ export default defineConfig({
   plugins: [
     react()
   ],
-  build: {
-    outDir: path.resolve(__dirname, 'dist'), // Set the output directory
-    emptyOutDir: true, // Clear the output directory before building
-    assetsDir: '.', // Serve assets from the root directory
-  },
 })
